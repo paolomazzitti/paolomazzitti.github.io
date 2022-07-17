1b61841f366c82ecc9c8ab5386582620cb0288b8
Paolo Mazzitti <pao.mazzitti@stud.uniroma3.it>
2022-07-17 17:03:14 +0200

    3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let theme = true
+
 function randomIntFromInterval() {
     let max = 200
     let min = 50
@@ -38,6 +40,24 @@ async function typing() {
 
 }
 
+document.querySelector("body").addEventListener("click", function() {
+    if (theme) {
+        document.querySelector("body").style.backgroundColor = "#282828"
+        document.querySelector(".typing").style.color = "#FAFAFA"
+        document.querySelector(".blinking-cursor").classList.add("blinking-cursor-light")
+        document.querySelector(".blinking-cursor").classList.remove("blinking-cursor")
+        theme = false
+    } else {
+        document.querySelector("body").style.backgroundColor = "#FAFAFA"
+        document.querySelector(".typing").style.color = "#282828"
+        document.querySelector(".blinking-cursor-light").classList.add("blinking-cursor")
+        document.querySelector(".blinking-cursor-light").classList.remove("blinking-cursor-light")
+        theme = true
+    }
+}, false)
+
 typing()
+
+
 
 
