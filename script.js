224d22a53c23811a8072d33e7a2b160a101c55bc
Paolo Mazzitti <pao.mazzitti@stud.uniroma3.it>
2022-07-17 15:50:23 +0200

    2

---
+++ script.js
@@ -0,0 +1,43 @@
+function randomIntFromInterval() {
+    let max = 200
+    let min = 50
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function deletePhrase() {
+    let text = document.querySelector(".typing").innerHTML;
+
+    while (text.length > 0) {
+        text = text.slice(0, -1)
+        document.querySelector(".typing").innerHTML = text;
+        await sleep(randomIntFromInterval())
+    }
+}
+
+async function writePhrase(phrase) {
+
+    let i = 0
+    while (i < phrase.length) {
+        await sleep(randomIntFromInterval())
+        document.querySelector(".typing").innerHTML += phrase.charAt(i);
+        i++;
+    }
+}
+
+async function typing() {
+    while (true) {
+        await writePhrase("hugo")
+        await sleep(randomIntFromInterval() * 10)
+        await deletePhrase()
+        await sleep(randomIntFromInterval() * 10)
+    }
+
+}
+
+typing()
+
+
